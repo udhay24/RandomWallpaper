c78a912ce6a6a468d434fab69463a584306a97d9
udhay24 <udhayakumar241998@gmail.com>
2018-11-22 20:39:55 +0530

    search activity

--- app/src/main/java/com/example/udhay/randomwallpaper/SearchActivity.java
+++ app/src/main/java/com/example/udhay/randomwallpaper/SearchActivity.java
@@ -4,16 +4,27 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import timber.log.Timber;
 
 public class SearchActivity extends AppCompatActivity {
+
+    SearchView searchView;
+    String message;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         handleIntent(getIntent());
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -24,8 +35,23 @@ public class SearchActivity extends AppCompatActivity {
 
     private void handleIntent(Intent intent) {
 
-        String message = intent.getStringExtra(SearchManager.QUERY);
+        message = intent.getStringExtra(SearchManager.QUERY);
         Timber.v("message: :" + message);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search_activity_menu, menu);
+
+        searchView = (SearchView) menu.findItem(R.id.search_menu_item).getActionView();
+        searchView.setIconifiedByDefault(false);
+        searchView.setIconified(false);
+        searchView.setQuery(message, false);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(this.getComponentName()));
+
+        return true;
     }
 }
