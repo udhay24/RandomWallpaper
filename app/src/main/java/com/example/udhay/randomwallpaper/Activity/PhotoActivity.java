7c3c46fad4ae0f9bda655230d340e979ce0fcbb5
udhay24 <udhayakumar241998@gmail.com>
2018-11-27 22:37:55 +0530

    added photo Activity

--- app/src/main/java/com/example/udhay/randomwallpaper/Activity/PhotoActivity.java
+++ app/src/main/java/com/example/udhay/randomwallpaper/Activity/PhotoActivity.java
@@ -1,25 +1,45 @@
 package com.example.udhay.randomwallpaper.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.udhay.randomwallpaper.R;
 import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PhotoActivity extends AppCompatActivity {
 
     String url;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
 
         url = getIntent().getStringExtra("url");
 
         ImageView imageView = findViewById(R.id.image);
 
+        Toast.makeText(this, "url " + url, Toast.LENGTH_SHORT).show();
         Picasso.get().load(url).into(imageView);
     }
+
+
 }
