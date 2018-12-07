489d25ac1c22d9b75645aa943f5c5e432c275167
udhay24 <udhayakumar241998@gmail.com>
2018-12-07 15:59:52 +0530

    added testing file

--- app/src/androidTest/java/com/example/udhay/randomwallpaper/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/udhay/randomwallpaper/ExampleInstrumentedTest.java
@@ -2,11 +2,26 @@ package com.example.udhay.randomwallpaper;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.udhay.randomwallpaper.activity.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +31,28 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivity = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.udhay.randomwallpaper", appContext.getPackageName());
+
+
     }
+
+    @Test
+    public void openSearch(){
+
+        onView(withId(R.id.search_menu_item))
+                .perform(click());
+
+        onView(withId(android.support.design.R.id.search_src_text)).perform(typeText("something"));
+
+    }
+
 }
