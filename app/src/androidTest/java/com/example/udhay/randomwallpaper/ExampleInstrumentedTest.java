eaa573b20c056f1b424996f59304908e8e2692ce
udhay24 <udhayakumar241998@gmail.com>
2018-12-08 23:19:16 +0530

    updated query method test method

--- app/src/androidTest/java/com/example/udhay/randomwallpaper/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/udhay/randomwallpaper/ExampleInstrumentedTest.java
@@ -3,8 +3,11 @@ package com.example.udhay.randomwallpaper;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.EspressoKey;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.udhay.randomwallpaper.activity.MainActivity;
 
@@ -12,12 +15,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getArguments;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -48,11 +56,35 @@ public class ExampleInstrumentedTest {
     @Test
     public void openSearch(){
 
+        String searchQuery = "search";
+
         onView(withId(R.id.search_menu_item))
                 .perform(click());
 
-        onView(withId(android.support.design.R.id.search_src_text)).perform(typeText("something"));
+        onView(withId(android.support.design.R.id.search_src_text))
+                .perform(typeText(searchQuery))
+                .perform(pressImeActionButton())
+                .check(matches(allOf(withId(android.support.design.R.id.search_src_text) , withText(searchQuery))));
 
     }
 
+    @Test
+    public void clickRecyclerItem(){
+
+        onView(withId(R.id.featured_images_recycler_view))
+                //.perform(RecyclerViewActions.scrollToPosition(20))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3 , click()))
+                .check(matches(allOf(withId(R.id.photo_image_view) , isDisplayed())));
+    }
+
+
+    @Test
+    public void clickCollection(){
+
+        onView(withId(R.id.view_pager))
+                .perform(swipeRight());
+
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.scrollToPosition(9));
+    }
 }
---
+++ app/src/androidTest/java/com/example/udhay/randomwallpaper/activity/MainActivityTest.java
@@ -0,0 +1,138 @@
+package com.example.udhay.randomwallpaper.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.udhay.randomwallpaper.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction textView = onView(
+                allOf(withText("NEW"),
+                        childAtPosition(
+                                allOf(withContentDescription("NEW"),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("NEW")));
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.search_menu_item), withContentDescription("search"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withId(R.id.search_src_text),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText("why"), closeSoftKeyboard());
+
+        ViewInteraction searchAutoComplete2 = onView(
+                allOf(withId(R.id.search_src_text), withText("why"),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete2.perform(click());
+
+        ViewInteraction searchAutoComplete3 = onView(
+                allOf(withId(R.id.search_src_text), withText("why"),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete3.perform(replaceText("whwgyy"));
+
+        ViewInteraction searchAutoComplete4 = onView(
+                allOf(withId(R.id.search_src_text), withText("whwgyy"),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete4.perform(closeSoftKeyboard());
+
+        ViewInteraction searchAutoComplete5 = onView(
+                allOf(withId(R.id.search_src_text), withText("whwgyy"),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete5.perform(pressImeActionButton());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
